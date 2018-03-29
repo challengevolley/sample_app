27bf2a54782802df731de7b767738afcc72a54f7
challengevolley <kazuhiko1979@gmail.com>
2018-03-29 06:04:27 +0000

    Implement advanced login

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -4,17 +4,12 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
   
    def setup
     @user = users(:michael)
-  end
+   end
 
-  test "logon with invalid information followed by logout" do
-  #ログイン用のパスを開く
+  test "login with valid information followed by logout" do
     get login_path
-  #新しいセッションのフォームが正しく表示されたことを確認する
-  #assert_template 'sessions/new'
-   post login_path, params: { session: { email:    @user.email,
+    post login_path, params: { session: { email:    @user.email,
                                           password: 'password' } }
-  #新しいセッションのフォームが再度表示され、フラッシュメッセージが追加されることを確認する
-    
     assert is_logged_in?
     assert_redirected_to @user
     follow_redirect!
@@ -22,19 +17,30 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", login_path, count: 0
     assert_select "a[href=?]", logout_path
     assert_select "a[href=?]", user_path(@user)
-    
     delete logout_path
     assert_not is_logged_in?
     assert_redirected_to root_url
+    # 2番目のウィンドウでログアウトをクリックするユーザーをシミュレートする
+    delete logout_path
     follow_redirect!
     assert_select "a[href=?]", login_path
     assert_select "a[href=?]", logout_path,      count: 0
     assert_select "a[href=?]", user_path(@user), count: 0
-    #assert_template 'sessions/new'
-    #assert_not flash.empty?
-    #別のページ (Homeページなど) にいったん移動する
-    #get root_path 
-    #移動先のページでフラッシュメッセージが表示されていないことを確認する
-    #assert flash.empty?
+  end
+  
+  test "login with remembering" do
+    log_in_as(@user, remember_me: '1')
+    #assert_not_empty cookies['remember_token']
+    assert_equal FILL_IN, assigns(:user).FILL_IN
+  end
+    
+  test "login without remembering" do
+    # クッキーを保存してログイン
+    log_in_as(@user, remember_me: '1')
+    delete logout_path
+    
+    # クッキーを削除してログイン
+    login_in_as(@user, remember_me: '0')
+    assert_empty cookies['remember_token']
   end
 end
